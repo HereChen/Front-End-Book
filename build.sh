d04c97b3b3a270f3d779c0d10e633dcfda382e6c
HereChen <chenlei.here@qq.com>
2018-01-30 14:28:14 +0800

    build: update script

--- build.sh
+++ build.sh
@@ -2,13 +2,6 @@
 markdown_section_folders=("fundamental" "frameworks" "solutions" "optimization" "tools")
 ignore_files=("READMD.md")
 
-# build sections
-
-# build */main.tex
-
-# build all
-
-# tools
 # delete file in current folder
 delete_by_extension() {
   find . -name $1 -type f -delete
@@ -18,6 +11,8 @@ delete_by_extension() {
 clean() {
   echo ============ clean compile temp files ============
   build_extensions=("*.aux" "*.bak" "*.log" "*.bbl" "*.dvi" "*.blg" "*.thm" "*.toc" "*.out" "*.lof" "*.lol" "*.lot" "*.fdb_latexmk" "*.synctex.gz" "*.bcf" "*.nav" "*.snm" "*.xdv" "*.vrb" "*.fls")
+
+  rm -rf .tmp
 
   # clean latex build
   for ext in ${build_extensions[@]}
@@ -29,36 +24,48 @@ clean() {
 }
 
 markdown_to_latex() {
-    mkdir -p .tmp/$1;
-    pandoc --listings $1/*.md -o .tmp/$1/content.tex
+  # .tmp
+  mkdir -p .tmp/$1;
+  pandoc --listings $1/*.md -o .tmp/$1/content.tex
 
-    cp src/format.cls .tmp/$1/
-    cp src/title.tex .tmp/$1/
-    cp src/main.tex .tmp/$1/
+  cp src/format.cls .tmp/$1/
+  cp src/title.tex .tmp/$1/
+  cp src/main.tex .tmp/$1/
+
+  # dist/*/latex
+  mkdir -p dist/$1/latex
+  cp -r .tmp/$1/* dist/$1/latex/
 }
 
 latex_to_pdf(){
-    mkdir -p dist/$1/pdf
-    xelatex  .tmp/$1/main.tex
-    xelatex  .tmp/$1/main.tex
-    cp .tmp/$1/*.pdf dist/pdf/
+  # dist/$1/pdf
+  cd .tmp/$1/
+  pwd
+  xelatex main.tex
+  xelatex main.tex
+  cd ../../
+  pwd
+
+  mkdir -p dist/$1/pdf
+  cp .tmp/$1/*.pdf dist/$1/pdf/
 }
 
 markdown_to_docx() {
-    mkdir -p dist/docx
-    pandoc -s src/markdown/*.md -o dist/docx/main.docx
+  mkdir -p dist/$1/docx
+  pandoc -s $1/*.md -o dist/$1/docx/main.docx
 }
 
 main() {
   clean
   for folder in ${markdown_section_folders[@]}
   do
+    echo ====== build ${folder} ======
     markdown_to_latex ${folder}
     latex_to_pdf ${folder}
-    # markdown_to_docx
+    markdown_to_docx ${folder}
+    echo ====== FINISHED build ${folder} ======
   done
 
-  cp .tmp/**/.pdf dist/
   rm -rf .tmp
 }
 
